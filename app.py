4b7bd5f4547dabe89431dbb31b4d8314b9548439
diodiodada <897665728@qq.com>
2020-03-03 14:32:16 +0800

    add index page

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask import url_for, escape
+from flask import render_template
 
-# ...
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -16,7 +29,6 @@ def user_page(name):
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
     print(url_for('user_page', name='peter'))  # 输出：/user/peter
